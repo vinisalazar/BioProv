c7fd4df3ce7079be97cac6aa15fb6f11248fad2e
Vini Salazar <viniws@gmail.com>
2020-06-26 12:55:49 -0300

    Adding methods and more tests for File class

--- bioprov/File.py
+++ bioprov/File.py
@@ -1,12 +1,35 @@
 """
 Files module containing classes File and SequenceFile.
 """
+from pathlib import Path
 
 
 class File:
     """
     Class for holding file and file information.
     """
+
+    def __init__(self, path, tag=None):
+        """
+        :param path: A UNIX-like file path.
+        :param tag: optional tag describing the file.
+        """
+        self.path = Path(path).absolute()
+        self.name = self.path.stem
+        self.directory = self.path.parent
+        self.extension = self.path.suffix
+        self.tag = tag
+        self.exists = self.path.exists()
+        self.size = get_size(self.path)
+        self.raw_size = get_size(self.path, convert=False)
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        self._size = value
 
     pass
 
@@ -17,3 +40,33 @@ class SequenceFile(File):
     """
 
     pass
+
+
+def convert_bytes(num):
+    """
+    Helper function to convert bytes into KB, MB, etc.
+
+    From https://stackoverflow.com/questions/2104080/how-can-i-check-file-size-in-python
+    """
+    for x in ["bytes", "KB", "MB", "GB", "TB"]:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
+
+
+def get_size(path, convert=True):
+    """
+    Calculate size of a given file.
+    :param path: Valid path of a file.
+    :param convert: Whether to convert the values to bytes, KB, etc.
+    :return: Size with converted values. 0 if file does not exist.
+    """
+    path = Path(path)
+    size = path.stat().st_size
+    if path.exists:
+        if convert:
+            return convert_bytes(size)
+        else:
+            return size
+    else:
+        return 0
--- tests/test_bioprov.py
+++ tests/test_bioprov.py
@@ -1,9 +1,18 @@
 """
-Testing module for the package
+Testing module for the package BioProv.
+
+Tests include:
+    - Imports;
+    - Class generation and methods.
 """
+from pathlib import Path
 
 
 def test_import_bioprov():
+    """
+    Test if the package can be imported correctly.
+    :return: ImportError if not able to import.
+    """
     try:
         import bioprov as bp
     except ImportError:
@@ -11,7 +20,34 @@ def test_import_bioprov():
 
 
 def test_import_classes():
+    """
+    Test if all classes can be imported correctly.
+    :return: ImportError if not able to import
+    """
     try:
         from bioprov import Config, File, SequenceFile
     except ImportError:
         raise
+
+
+def test_File():
+    """
+    Test if the File class works as expected.
+    :return:
+    """
+    import bioprov as bp
+
+    file, tag = bp.__file__, "Init file for BioProv."
+    f = bp.File(file, tag)
+    attributes = {
+        "path": f.path == Path(file).absolute(),
+        "name": f.name == f.path.stem,
+        "dir": f.directory == f.path.parent,
+        "extension": f.extension == f.path.suffix,
+        "tag": f.tag == tag,
+        "exists": f.exists,
+        "size": float(f.size.split()[0]),  # Convert string to float
+        "raw_size": f.raw_size,  # See if larger than 0
+    }
+    for k, statement in attributes.items():
+        assert statement, f"{k} did not pass!"
