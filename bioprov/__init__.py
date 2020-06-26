1891918166a0392875f874284a6a0940658246e5
Vini Salazar <viniws@gmail.com>
2020-06-26 16:03:23 -0300

    Adding config variable

--- bioprov/__init__.py
+++ bioprov/__init__.py
@@ -1,5 +1,6 @@
-name = "bioprov"
-
-from .Config import Config
+from .Config import Config, config
 from .File import File, get_size, convert_bytes
 from .SequenceFile import SequenceFile, seqrecordgenerator
+from .data import data_dir, genomes_dir
+
+name = "bioprov"
