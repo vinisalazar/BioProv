5cdda0398710dbd17167a0aea62485e4e9d8d0d4
Vini Salazar <viniws@gmail.com>
2020-06-26 13:25:08 -0300

    Add tests and imports for more functions in File module

--- bioprov/__init__.py
+++ bioprov/__init__.py
@@ -1,4 +1,4 @@
 name = "bioprov"
 
 from .Config import Config
-from .File import File, SequenceFile
+from .File import File, SequenceFile, get_size, convert_bytes
--- tests/test_bioprov.py
+++ tests/test_bioprov.py
@@ -27,16 +27,17 @@ def test_import_classes():
     :return: ImportError if not able to import
     """
     try:
-        from bioprov import Config, File, SequenceFile
+        from bioprov import Config, File, SequenceFile, get_size
     except ImportError:
         raise
 
 
 def test_File():
     """
-    Test if the File class works as expected.
-    Tests attributes for existing file.
-    Tests if attributes are false for non-existing file.
+    Tests objects in the File module:
+        - existing File
+        - non-existing File
+        - get_size(), convert_bytes()
     :return:
     """
     import bioprov as bp
@@ -47,16 +48,19 @@ def test_File():
     non_existing = randomString() + "." + randomString(3)
     nf = bp.File("./" + non_existing)
     attributes = {
+        # File class - existing file
         "path": f.path == Path(file).absolute(),
         "name": f.name == f.path.stem,
         "dir": f.directory == f.path.parent,
         "extension": f.extension == f.path.suffix,
         "tag": f.tag == tag,
-        "exists": f.exists,
-        "size": float(f.size.split()[0]),  # Convert string to float
-        "raw_size": f.raw_size,  # See if larger than 0
+        "exists": f.exists is True,
+        "get_size": f.size == bp.get_size(f.path),  # get_size() function
+        "raw_get_size": f.raw_size
+        == bp.get_size(f.path, convert=False),  # get_size(convert=False)
         "non_existing": nf.exists is False,
-        "no_size": nf.size is False,
+        "no_size": nf.size is 0,
+        "convert_bytes": bp.convert_bytes(2 ** 10) == "1.0 KB",
     }
     for k, statement in attributes.items():
         assert statement, f"{k} did not pass!"
