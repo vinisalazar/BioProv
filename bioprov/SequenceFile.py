d805dea1074c5178a67dc637dfa8fc6bbb844e19
Vini Salazar <viniws@gmail.com>
2020-06-26 16:03:42 -0300

    Implementing records property

--- bioprov/SequenceFile.py
+++ bioprov/SequenceFile.py
@@ -12,8 +12,24 @@ class SequenceFile(File):
     """
 
     def __init__(self, path, tag=None):
+        """
+        :param path: A UNIX-like file path.
+        :pram tag: optional tag describing the file.
+        """
         File.__init__(self, path, tag)
         self.records = seqrecordgenerator(self.path)
+
+    @property
+    def records(self):
+        return self._records
+
+    @records.setter
+    def records(self, value):
+        try:
+            self._records = SeqIO.to_dict(value)
+        except TypeError:  # in case the file does not exist
+            print(f"File {self.path} does not exist!")
+            self._records = value
 
     pass
 
@@ -23,4 +39,9 @@ def seqrecordgenerator(path):
     :param path: Path to a valid FASTA file.
     :return: A SeqIO SeqRecords generator.
     """
-    return SeqIO.parse(path, format="fasta")
+    try:
+        records = SeqIO.parse(path, format="fasta")
+        return records
+    except FileNotFoundError as e:
+        print(e)
+        return None
