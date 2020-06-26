c3e32d3fa04189f53dc871a647e86afa315ead3f
Vini Salazar <viniws@gmail.com>
2020-06-26 16:02:49 -0300

    Fixing imports in Config module and creating config variable

--- bioprov/Config.py
+++ bioprov/Config.py
@@ -1,7 +1,19 @@
 """
-Configuration module.
+Contains the Config class.
 """
+from .data import data_dir, genomes_dir
 
 
 class Config:
+    """
+    Class to define package level variables and settings.
+    """
+
+    def __init__(self):
+        self.data_dir = data_dir
+        self.genomes_dir = genomes_dir
+
     pass
+
+
+config = Config()
