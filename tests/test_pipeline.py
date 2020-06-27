ebaae77197aa320b892a08a91b9943fca2f249d9
Vini Salazar <viniws@gmail.com>
2020-06-26 22:05:11 -0300

    Add to-dos

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,3 +1,11 @@
+"""
+Testing module for drafting new ideas.
+
+To do:
+    - create classes Sample, Process
+    - create 'kind' attribute in Parameter class
+"""
+
 import bioprov as bp
 from subprocess import Popen, PIPE
 
@@ -6,16 +14,19 @@ def test_pipeline():
     genome_path = str(bp.genomes_dir) + "/GCF_000010065.1_ASM1006v1_genomic.fna"
     tag = "Synechococcus elongatus PCC 6301"
     genome = bp.SequenceFile(genome_path, tag)
+    # sample = Sample(tag, files={"assembly": genome})
     assert genome.exists
     program, version = "prodigal", "v2.6.3"
     program = bp.Program(program, version=version)
-    param_i = bp.Parameter("-i", genome_path)
-    param_a = bp.Parameter("-a", str(genome.path).replace(".fna", "_proteins.faa"))
+    param_i = bp.Parameter("-i", genome_path, description="assembly")  # , kind='input')
+    param_a = bp.Parameter(
+        "-a", str(genome.path).replace(".fna", "_proteins.faa"), description="proteins"
+    )  # , kind='output')
     program.add_parameter(param_i)
     program.add_parameter(param_a)
     p = Popen(program.cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    (child_stdin, child_stdout, child_stderr) = (p.stdin, p.stdout, p.stderr)
-    return genome, program, p, (child_stdin, child_stdout, child_stderr)
+    (stdin, stdout, stderr) = (p.stdin, p.stdout, p.stderr)
+    return genome, program, p, (stdin, stdout, stderr)
 
 
-genome, program, p, (child_stdin, child_stdout, child_stderr) = test_pipeline()
+genome, program, p, (stdin, stdout, stderr) = test_pipeline()
