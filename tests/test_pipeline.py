d480470c344746ea5bf47b378405c68d5923bcf4
Vini Salazar <viniws@gmail.com>
2020-06-30 14:26:24 -0300

    Tests passing

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -12,28 +12,31 @@ from subprocess import Popen, PIPE
 def test_pipeline():
     assembly_file_path = str(bp.genomes_dir) + "/GCF_000010065.1_ASM1006v1_genomic.fna"
     name, tag = "Synechococcus elongatus PCC 6301", "GCF_000010065.1"
-    assembly_file = bp.SequenceFile(assembly_file_path, tag)
+    assembly_file = bp.SequenceFile(assembly_file_path, "assembly")
+    protein_file = bp.File(
+        assembly_file_path.replace(".fna", "_proteins.faa"), "proteins"
+    )
     sample = bp.Sample(
         name,
         tag,
         {"assembly": assembly_file},
         {"description": f"Genome of {name} with RefSeq accession {tag}"},
     )
+    sample.add_file(protein_file)
     assert assembly_file.exists, sample.files["assembly"].exists
     program, version = "prodigal", "v2.6.3"
     program = bp.Program(program, version=version)
-    param_i = bp.Parameter("-i", assembly_file, description="assembly", kind="input")
+    param_i = bp.Parameter(
+        "-i", str(assembly_file), description="assembly", kind="input"
+    )
     param_a = bp.Parameter(
-        "-a",
-        str(genome.path).replace(".fna", "_proteins.faa"),
-        description="proteins",
-        kind="output",
+        "-a", str(protein_file), description="proteins", kind="output",
     )
     program.add_parameter(param_i)
     program.add_parameter(param_a)
     p = Popen(program.cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     (stdin, stdout, stderr) = (p.stdin, p.stdout, p.stderr)
-    return genome, program, p, (stdin, stdout, stderr)
+    return sample, program, p, (stdin, stdout, stderr)
 
 
 genome, program, p, (stdin, stdout, stderr) = test_pipeline()
