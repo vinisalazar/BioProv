e65c86294ea7975410159d79e4e08f3c365b28b9
Vini Salazar <viniws@gmail.com>
2020-06-30 14:26:12 -0300

    Added Sample import

--- bioprov/__init__.py
+++ bioprov/__init__.py
@@ -2,6 +2,7 @@ from .Config import Config, config
 from .data import data_dir, genomes_dir
 from .File import File, get_size, convert_bytes
 from .Program import Program, Parameter
+from .Sample import Sample
 from .SequenceFile import SequenceFile, seqrecordgenerator
 
 name = "bioprov"
