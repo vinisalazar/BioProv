7900c3a9663b5b308d48745b6d77d068d29e1343
Vini Salazar <viniws@gmail.com>
2020-07-02 16:02:51 -0300

    Added test_Sample

---
+++ tests/test_Sample.py
@@ -0,0 +1,27 @@
+"""
+Testing for the Sample module.
+"""
+from bioprov.Sample import Sample
+from bioprov.Config import config
+
+
+def test_Sample():
+    """
+    Testing for the Sample class.
+    :return:
+    """
+    attributes = {
+        "name": "GCF_000010065.1_ASM1006v1",
+        "tag": "Synechococcus elongatus PCC 6301",
+        "files": dict(),
+        "attributes": {"habitat": "freshwater"},
+    }
+    s = Sample()
+    for attr, v in attributes.items():
+        setattr(s, attr, v)
+        assert getattr(s, attr) == v
+
+    assembly_path = config.genomes_dir + s.name + "_genomic.fna"
+    s.add_files({"assembly": assembly_path})
+    s.add_files({"proteins": assembly_path.replace(".fna", ".faa")})
+    assert s.files["assembly"].exists, f"Couldn't find file in path {assembly_path}."
