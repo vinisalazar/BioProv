fcf13b7cc24fc8bdfd8e2e048d7e191dbe6145fc
Vini Salazar <viniws@gmail.com>
2020-07-03 11:32:37 -0300

    Added imports to data package

--- bioprov/data/__init__.py
+++ bioprov/data/__init__.py
@@ -6,6 +6,8 @@ from pathlib import Path
 
 data_dir = Path(__file__).parent
 genomes_dir = Path.joinpath(data_dir, "genomes")
+datasets_dir = Path.joinpath(data_dir, "datasets")
 synechococcus_genome = Path.joinpath(
     genomes_dir, "GCF_000010065.1_ASM1006v1_genomic.fna"
 )
+synechococcus_dataset = Path.joinpath(datasets_dir, "synechococcus.csv")
