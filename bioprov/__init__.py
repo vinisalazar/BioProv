cd4acc59da49216d5321948e46873d1f114dff73
Vini Salazar <viniws@gmail.com>
2020-07-03 20:47:25 -0300

    Exclude data objects from bioprov/__init__.py, bioprov.data must be specified.

--- bioprov/__init__.py
+++ bioprov/__init__.py
@@ -9,6 +9,5 @@ from .File import File
 from .Program import Program, Parameter, Run
 from .Sample import Sample, SampleSet, read_csv, from_df
 from .SequenceFile import SequenceFile, seqrecordgenerator
-from .utils import random_string, convert_bytes, get_size
 
 name = "bioprov"
