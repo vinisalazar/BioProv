026fa3703b357399197c49c488213fc6a92e0eeb
Vini Salazar <viniws@gmail.com>
2020-07-03 12:13:17 -0300

    Added imports

--- bioprov/__init__.py
+++ bioprov/__init__.py
@@ -8,7 +8,7 @@ from .Config import Config, config
 from .data import data_dir, genomes_dir, synechococcus_genome
 from .File import File
 from .Program import Program, Parameter, Run
-from .Sample import Sample
+from .Sample import Sample, SampleSet, read_csv, from_df
 from .SequenceFile import SequenceFile, seqrecordgenerator
 from .utils import random_string, convert_bytes, get_size
 
