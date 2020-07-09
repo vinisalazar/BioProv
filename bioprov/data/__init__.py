cab2537e59d3948e45fab3f557155f2be16b82ad
Vini Salazar <viniws@gmail.com>
2020-07-08 22:50:09 -0300

    Adding genome_annotation dataset

--- bioprov/data/__init__.py
+++ bioprov/data/__init__.py
@@ -10,4 +10,8 @@ datasets_dir = Path.joinpath(data_dir, "datasets")
 synechococcus_genome = Path.joinpath(
     genomes_dir, "GCF_000010065.1_ASM1006v1_genomic.fna"
 )
+
+# To-do: Join these two as the same
+# Refactor genome_annotation so it takes a dataset same as 'synechococcus.csv'
 synechococcus_dataset = Path.joinpath(datasets_dir, "synechococcus.csv")
+genome_annotation_dataset = Path.joinpath(datasets_dir, "genome_annotation_input.tsv")
