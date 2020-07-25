b28e242b92daa4dba028f68dcd2f7fbd1acb4e2a
Vini Salazar <viniws@gmail.com>
2020-07-24 22:48:01 -0300

    Add headers to all project files

--- bioprov/programs/__init__.py
+++ bioprov/programs/__init__.py
@@ -1,1 +1,8 @@
+__author__ = "Vini Salazar"
+__license__ = "MIT"
+__maintainer__ = "Vini Salazar"
+__url__ = "https://github.com/vinisalazar/bioprov"
+__version__ = "0.1.0"
+
+
 from .programs import prodigal, prokka, kaiju, kaiju2table
