b34024a7ca137582c3968f7544e4f589fdd49894
Vini Salazar <viniws@gmail.com>
2020-07-27 19:13:24 -0300

    Developing workflow class

--- bioprov/src/Workflow.py
+++ bioprov/src/Workflow.py
@@ -8,6 +8,8 @@ __version__ = "0.1.0"
 """
 Contains the Workflow class and related functions.
 """
+
+import argparse
 import pandas as pd
 from os import path
 from bioprov.utils import warnings
@@ -45,7 +47,8 @@ class Workflow:
         :param input_: Input of workflow. May be a directory or a tab-delimited file.
         :param input_type: Input type of the workflow. Choose from ('directory', 'dataframe')
         :param index_col: Name of index column which will define sample names if input_type is 'dataframe'.
-        :param file_columns: Name of columns containing files if input_type is 'dataframe'. Name of file tag if input_type is 'directory'.
+        :param file_columns: Name of columns containing files if input_type is 'dataframe'.
+                             Name of file tag if input_type is 'directory'.
         :param file_extensions: Extension of files if input_type is 'directory'.
         :param steps: Iterator of instances of bioprov.Program to run on each sample.
         :param parser: argparse.ArgumentParser object used to construct the workflow's command-line application.
@@ -71,9 +74,17 @@ class Workflow:
         self.success = 0
         self.kwargs = kwargs
         self.sampleset = None
+        self.parser = None
 
+        # Only generate sampleset if there is an input.
         if self.input is not None:
             self.generate_sampleset()
+
+        # Only generate parser if there is a name, description, and steps.
+        if all(
+            (item is not None for item in (self.name, self.description, self.steps))
+        ):
+            self.parser = self.generate_parser()
 
     def generate_sampleset(self):
         """
@@ -85,7 +96,67 @@ class Workflow:
             "directory": self._load_directory_input(),
         }
 
-        _generate_sampleset[self.input_type]
+        self.sampleset = _generate_sampleset[self.input_type]
+
+    def generate_parser(self):
+        parser = argparse.ArgumentParser(self.name, description=self.description)
+        parser.add_argument(
+            "-i",
+            "--input",
+            help="""
+            Input file, may be a tab delimited file or a directory.\
+            If a file, must contain column '{}' for sample ID and '{}' for files.\
+            See program help for information.
+            """.format(
+                self.index_col, self.file_columns
+            ),
+        )
+        parser.add_argument()
+        return parser
+
+    def _sampleset_from_dataframe(self, df):
+        """
+        Run from_df on dataframe and updates self.sampleset.
+        :param df: Instance of pd.DataFrame.
+        :return: Updates self.sampleset.
+        """
+        # Loading samples statement
+        print(warnings["sample_loading"](len(df)))
+        sampleset = from_df(
+            df, index_col=self.index_col, file_cols=self.file_columns, tag=self.tag
+        )
+        return sampleset
+
+    def _load_directory_input(self):
+        """
+        Generates SampleSet from directory.
+        :return: bioprov.SampleSet
+        """
+        directory = self.input
+        file_extensions = self.file_extensions
+        file_columns = self.file_columns
+
+        assert path.isdir(directory), "Input directory '{}' not found.".format(
+            directory
+        )
+
+        # Get files with correct extensions from directory
+        if isinstance(file_extensions, str):
+            file_extensions = (file_extensions,)
+            self.file_extensions = file_extensions
+        files = []
+        for ext in file_extensions:
+            files += glob(path.join(directory, "*." + ext))
+
+        # Build dataframe from files
+        df = pd.DataFrame(files)
+        df.columns = (file_columns,)
+        df["sample-id"] = df[file_columns].apply(
+            lambda s: path.splitext(path.basename(s))[0]
+        )
+
+        sampleset = self._sampleset_from_dataframe(df)
+        return sampleset
 
     def _load_dataframe_input(self):
         """
@@ -122,50 +193,5 @@ class Workflow:
                     file_
                 )
 
-        # Build sample set from dataframe
-        self._sampleset_from_dataframe(df)
-
-    def _sampleset_from_dataframe(self, df):
-        """
-        Run from_df on dataframe and updates self.sampleset.
-        :param df: Instance of pd.DataFrame.
-        :return: Updates self.sampleset.
-        """
-        # Loading samples statement
-        print(warnings["sample_loading"](len(df)))
-        sampleset = from_df(
-            df, index_col=self.index_col, file_cols=self.file_columns, tag=self.tag
-        )
-        self.sampleset = sampleset
-
-    def _load_directory_input(self):
-        """
-        Generates SampleSet from directory.
-        :return: bioprov.SampleSet
-        """
-        directory = self.input
-        file_extensions = self.file_extensions
-        file_columns = self.file_columns
-
-        assert path.isdir(directory), "Input directory '{}' not found.".format(
-            directory
-        )
-
-        # Get files with correct extensions from directory
-        if isinstance(file_extensions, str):
-            file_extensions = (file_extensions,)
-            self.file_extensions = file_extensions
-
-        files = []
-        for ext in file_extensions:
-            files.append(glob(path.join(directory, "*." + ext)))
-
-        # Build dataframe from files
-        df = pd.DataFrame(files)
-        df.columns = (file_columns,)
-        df["sample-id"] = df[file_columns].apply(
-            lambda s: path.splitext(path.basename(s))[0]
-        )
-
-        # Build sample set from dataframe
-        self._sampleset_from_dataframe(df)
+        sampleset = self._sampleset_from_dataframe(df)
+        return sampleset
