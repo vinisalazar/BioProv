4b2b4224ca573eba28895d64f7dbb24c6367a8fd
Vini Salazar <viniws@gmail.com>
2020-08-26 12:14:01 -0300

    Update dependencies

--- setup.py
+++ setup.py
@@ -29,5 +29,5 @@ setuptools.setup(
     scripts=["bioprov/cli/bioprov", "bioprov/workflows/genome_annotation.py"],
     include_package_data=True,
     python_requires=">=3.6",
-    install_requires=["biopython", "tqdm", "pandas", "pytest", "prov", "coolname"],
+    install_requires=["biopython", "tqdm", "pandas", "pytest", "prov", "pydot"],
 )
