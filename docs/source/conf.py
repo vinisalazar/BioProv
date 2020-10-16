12ad4d66cf443d2e80ce687f7b875fbc558a568e
Vini Salazar <viniws@gmail.com>
2020-10-16 19:17:42 -0300

    Wild attempts to fix documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,6 +58,8 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+master_doc = "index"
+
 
 def skip(app, what, name, obj, would_skip, options):
     if name == "__init__":
