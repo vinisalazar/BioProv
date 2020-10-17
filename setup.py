6860a31f59ec472537be9486b9489ac01f0f05d8
Vini Salazar <viniws@gmail.com>
2020-10-16 22:05:38 -0300

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ __author__ = "Vini Salazar"
 __license__ = "MIT"
 __maintainer__ = "Vini Salazar"
 __url__ = "https://github.com/vinisalazar/bioprov"
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 
 import setuptools
@@ -12,7 +12,7 @@ with open("README.md", "r") as readme_file:
 
 setuptools.setup(
     name="bioprov",
-    version="0.1.7",
+    version="0.1.8",
     author="Vini Salazar",
     author_email="viniws@gmail.com",
     description="BioProv - Provenance capture for bioinformatics workflows",
