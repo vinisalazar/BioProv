e9843dd94b81603dca2f1173be02d17116f71406
Vini Salazar <viniws@gmail.com>
2020-10-27 10:30:40 -0300

    Bump version number

--- bioprov/src/main.py
+++ bioprov/src/main.py
@@ -2,7 +2,7 @@ __author__ = "Vini Salazar"
 __license__ = "MIT"
 __maintainer__ = "Vini Salazar"
 __url__ = "https://github.com/vinisalazar/bioprov"
-__version__ = "0.1.14"
+__version__ = "0.1.15"
 
 """
 
