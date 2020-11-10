71f78a02086d02d707246d424843dd1695f5c44a
Vini Salazar <viniws@gmail.com>
2020-11-09 23:52:22 -0300

    Bumping version number

--- bioprov/programs/__init__.py
+++ bioprov/programs/__init__.py
@@ -2,7 +2,7 @@ __author__ = "Vini Salazar"
 __license__ = "MIT"
 __maintainer__ = "Vini Salazar"
 __url__ = "https://github.com/vinisalazar/bioprov"
-__version__ = "0.1.19"
+__version__ = "0.1.20"
 
 
 from .programs import prodigal, diamond, blastn, blastp, prokka, kaiju, kaiju2table
