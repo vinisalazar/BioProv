dc487a01e5142b5dd515ad3fec85ca9762ca8540
Vini Salazar <viniws@gmail.com>
2020-12-16 17:02:25 -0300

    Add files to run example project

---
+++ paper/example/myProject.py
@@ -0,0 +1,35 @@
+import bioprov as bp
+
+project = bp.read_csv(
+    "myTable.csv",
+    file_cols="report",
+    sequencefile_cols="assembly",
+    tag="myProject",
+    import_data=True,
+)
+
+project.files
+
+project["sample_1"]
+
+project["sample_1"].files
+
+project["sample_1"].attributes
+
+project["sample_1"].files["assembly"].GC
+
+from bioprov.programs import prodigal
+
+with project["sample_1"] as sample:
+    sample.add_programs(prodigal(sample))
+    sample.run_programs()
+
+project.to_csv()  # exports in tabular format
+
+project.to_json()  # exports as JSON
+
+project.update_db()
+
+project = bp.load_project("myProject")  # call projects by their tag
+
+project.auto_update = True
