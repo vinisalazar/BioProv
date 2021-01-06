09f6fc4e3ee06628b1cef3eb9f266e5e29d419ed
Vini Salazar <viniws@gmail.com>
2021-01-06 20:24:05 -0300

    Add workflow code

---
+++ use-case/workflow.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import bioprov as bp
+from bioprov.programs import prodigal
+
+
+# preprocessing
+def download_data(input_file):
+
+    metadata = input_file.replace(".txt", "_metadata.txt")
+
+    ncbi_dl = bp.Program("ncbi-genome-download")
+
+    params = [
+        bp.Parameter("-F", "fasta,assembly-report"),
+        bp.Parameter("-s", "genbank"),
+        bp.Parameter("-A", input_file),
+        bp.Parameter("-o", "data/"),
+        bp.Parameter("-m", metadata),
+        bp.Parameter("bacteria")
+    ]
+
+    for param in params:
+        ncbi_dl.add_parameter(param)
+
+    ncbi_dl.run()
+
+    return ncbi_dl, metadata
+
+
+def sort(metadata):
+
+    sort = bp.Program("sort")
+
+    params = [
+        bp.Parameter("-t", "' '"),
+        bp.Parameter("-k", "1,1"),
+        bp.Parameter("-u", metadata),
+        bp.Parameter("-r"),
+        bp.Parameter("-o", metadata)
+    ]
+
+    for param in params:
+        sort.add_parameter(param)
+
+    sort.run()
+
+    return sort
+
+
+def gunzip():
+    gunzip = bp.Program("gunzip")
+    gunzip.add_parameter(bp.Parameter("data/genbank/bacteria/*/*"))
+    gunzip.run(suppress_stdout=True)
+    return gunzip
+
+
+def sed_gz(metadata):
+    sed_gz = bp.Program("sed")
+    params = [
+        bp.Parameter("-i", ".bkp"),
+        bp.Parameter("'s/.gz//g'"),
+        bp.Parameter(metadata),
+    ]
+
+    for param in params:
+        sed_gz.add_parameter(param)
+    sed_gz.run()
+
+    return sed_gz
+
+
+def sed_column(metadata):
+    sed_column = bp.Program("sed")
+
+    params = [
+        bp.Parameter("-i", "bkp"),
+        bp.Parameter("'s/local_filename/genome_assembly/g'"),
+        bp.Parameter(str(metadata)),
+    ]
+
+    for param in params:
+        sed_column.add_parameter(param)
+
+    sed_column.run()
+
+    return sed_column
+
+
+def load_project(tag, metadata, *args):
+    proj = bp.read_csv(metadata,
+                       sep="\t",
+                       sequencefile_cols="genome_assembly",
+                       import_data=True,
+                       tag=tag)
+
+    proj.add_programs(*args)
+
+    return proj
+
+
+# processing
+
+# gene calling
+def prodigal(proj):
+    for sample in proj:
+        p = prodigal(input_tag="genome_assembly")
+        del p.output_files["-s"]
+        p.create_func(sample)
+        sample.add_programs(p)
+        sample.run_programs()
+
+# pairwise gene comparison
+def fastani(proj):
+    fastani_input = bp.File(f"data/fastani_input_{proj.tag}.txt", tag="fastani_input")
+    fastani_output = bp.File(f"data/fastani_output_{proj.tag}.txt", tag="fastani_output")
+    proj.add_files([fastani_input, fastani_output])
+    with open(proj.files["fastani_input"].path, "w") as f:
+        for file in (sample.files["genome_assembly"] for k, sample in proj.items()):
+            f.write(str(file) + "\n")
+
+    fastani = bp.Program("fastani")
+
+    params = [
+        bp.Parameter("--refList", proj.files["fastani_input"], kind="input"),
+        bp.Parameter("--queryList", proj.files["fastani_input"]),
+        bp.Parameter("--threads", bp.config.threads),
+        bp.Parameter("--fragLen", 200),
+        bp.Parameter("--minFraction", 0.01),
+        bp.Parameter("--output", proj.files["fastani_output"], kind="output")
+    ]
+
+    for param in params:
+        fastani.add_parameter(param)
+
+    proj.add_programs(fastani)
+    fastani.run()
+
+
+# format output
+def format_fastani_output(proj):
+    ffo_f = bp.File("data/fastani_output_fmt.txt")
+    proj.add_files(ffo_f)
+
+    ffo_p = bp.Program("format_fastani_output")
+    ffo_p.found = True
+
+    params = [
+        bp.Parameter("python", "format_fastani_output.py"),
+        bp.Parameter("-i", proj.files["fastani_output"], kind="input"),
+        bp.Parameter("-o", proj.files["fastani_output_fmt"], kind="output"),
+        bp.Parameter("--invert")
+    ]
+
+    for param in params:
+        ffo_p.add_parameter(param)
+
+    proj.add_programs(ffo_p)
+    ffo_p.run()
+
+
+# create labels
+def labels(proj):
+    labels = bp.File(f"data/labels_{proj.tag}.txt")
+    proj.add_files(labels)
+    with open(labels.path, "w") as f:
+        for sample in proj:
+            filename = str(sample.files["genome_assembly"].basename)
+            label = sample.attributes["organism_name"]
+            f.write(f"{filename},{label}\n")
+
+# cluster and plot
+def cluster(proj):
+    dendrogram = bp.File(f"data/dendrogram_{proj.tag}.pdf")
+    proj.add_files(dendrogram)
+
+    clust = bp.Program("cluster_and_plot")
+    clust.found = True
+
+    params = [
+        bp.Parameter("python", "hierarchical_clustering.py"),
+        bp.Parameter("-t", proj.files["fastani_output_fmt"], kind="input"),
+        bp.Parameter("-l", proj.files["labels"], kind="misc"),
+        bp.Parameter("-o", proj.files["dendrogram"], kind="output"),
+        bp.Parameter("-c", 22.5, kind="misc")
+    ]
+
+    for param in params:
+        clust.add_parameter(param)
+
+    proj.add_programs(clust)
+    clust.run()
+
+
+def export_provenance(proj):
+    prov = bp.BioProvDocument(proj)
+    prov.dot.write_pdf(f"graphs_{proj.tag}")
+    prov.write_provn()
+
+
+# In[6]:
+
+
+def preprocessing(input_file, tag):
+    ncbi_dl, metadata = download_data(input_file)
+    sort = sort(metadata)
+    gunzip = gunzip()
+    sed_gz = sed_gz(metadata)
+    sed_column = sed_column(metadata)
+    proj = load_project(tag, metadata, ncbi_dl, sort, gunzip, sed_gz, sed_column)
+    proj.auto_update = True
+
+
+def processing(tag):
+    proj = bp.load_project(tag)
+    prodigal(proj)
+    fastani(proj)
+    format_fastani_output(proj)
+    labels(proj)
+    cluster(proj)
+    export_prov(proj)
+
+
+
+# In[ ]:
+
+
+
+
