7b65bbfa17a22ac7213b41a80c132c56db380325
Vini Salazar <viniws@gmail.com>
2021-01-08 13:50:19 -0300

    Bump version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = "2020, Vini Salazar"
 author = "Vini Salazar"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.22"
+release = "0.1.23"
 
 
 # -- General configuration ---------------------------------------------------
