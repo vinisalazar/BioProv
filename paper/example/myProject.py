9d45e8748928e94706e82de03851ba50b76be4d0
Vini Salazar <viniws@gmail.com>
2021-03-08 11:01:24 -0300

    Printing more versions of provenance graph

--- paper/example/myProject.py
+++ paper/example/myProject.py
@@ -1,34 +1,36 @@
 import bioprov as bp
 
-# project = bp.read_csv(
-#     "myTable.csv",
-#     file_cols="report",
-#     sequencefile_cols="assembly",
-#     tag="myProject",
-#     import_data=True,
-# )
+project = bp.read_csv(
+    "myTable.csv",
+    file_cols="report",
+    sequencefile_cols="assembly",
+    tag="myProject",
+    import_data=True,
+)
 
-# project.files
+project.files
 
-# project["sample_1"]
+project["sample_1"]
 
-# project["sample_1"].files
+project["sample_1"].files
 
-# project["sample_1"].attributes
+project["sample_1"].attributes
 
-# project["sample_1"].files["assembly"].GC
+project["sample_1"].files["assembly"].GC
 
-# from bioprov.programs import prodigal
+del project["sample_2"]
 
-# with project["sample_1"] as sample:
-#     sample.add_programs(prodigal(sample))
-#     sample.run_programs()
+from bioprov.programs import prodigal
 
-# project.to_csv()  # exports in tabular format
+with project["sample_1"] as sample:
+    sample.add_programs(prodigal(sample))
+    sample.run_programs()
 
-# project.to_json()  # exports as JSON
+project.to_csv()  # exports in tabular format
 
-# project.update_db()
+project.to_json()  # exports as JSON
+
+project.update_db()
 
 project = bp.load_project("myProject")  # call projects by their tag
 
@@ -38,4 +40,4 @@ prov = bp.BioProvDocument(project, add_users=False)
 
 prov.write_provn()
 
-prov.dot.write_pdf("myProject.pdf")
+prov.dot.write_pdf("myProject_nousers.pdf")
