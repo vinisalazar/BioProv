66fb5d4b5ed809376cf48212cd71f312b9074579
jvfe <jvfecav@gmail.com>
2021-05-07 14:11:42 -0300

    docs: Fix top-level docstrings for programs

--- bioprov/programs/programs.py
+++ bioprov/programs/programs.py
@@ -1,14 +1,12 @@
+"""
+Module for holding preset instances of the Program class.
+"""
 __author__ = "Vini Salazar"
 __license__ = "MIT"
 __maintainer__ = "Vini Salazar"
 __url__ = "https://github.com/vinisalazar/bioprov"
 __version__ = "0.1.24"
 
-
-"""
-Module for holding preset instances of the Program class.
-Module for holding preset instances of the Program class.
-"""
 
 from os import path
 from pathlib import Path
