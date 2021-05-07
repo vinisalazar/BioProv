cab02362141422f6bfa4261bb2ac9562a4f29614
jvfe <jvfecav@gmail.com>
2021-05-07 14:10:33 -0300

    docs: Fix top-level docstrings for data

--- bioprov/data/__init__.py
+++ bioprov/data/__init__.py
@@ -1,13 +1,12 @@
+"""
+Contains example and test data.
+"""
 __author__ = "Vini Salazar"
 __license__ = "MIT"
 __maintainer__ = "Vini Salazar"
 __url__ = "https://github.com/vinisalazar/bioprov"
 __version__ = "0.1.24"
 
-
-"""
-Contains example and test data.
-"""
 
 from pathlib import Path
 
